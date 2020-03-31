e553550bccebac5a5c0714c23f88e0de381e7429
lookrecius <bragin.boris@ya.ru>
2020-03-31 05:04:29 +0300

    Added export variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
 	import ContactCard from './ContactCard.svelte';
 	let name = "Bo";
+	let title ="";
+	let image ="";
 	let age = 30;
+	let description ="";
+
 
 	$: uppercaseName = name.toUpperCase();
 
@@ -31,10 +35,17 @@
 	<!-- <button on:click="{changeName}">Change Name</button> -->
 	<!-- <input type="text" value={name} on:input={nameInput} /> -->
 	<input type="text" bind:value={name} />
+	<input type="text" bind:value={title} />
+	<input type="text" bind:value={image} />
+	<textarea bind:value={description} rows="5"></textarea>
 
 
 
-	<ContactCard userName="{name}"/>
+	<ContactCard
+			userName={name}
+			jobTitle={title}
+			description={description}
+			userImage={image}/>
 
 </main>
 
